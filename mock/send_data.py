9ea885a0e7dc22195479120bec88731ecff88cbc
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2016-05-23 13:29:05 +0300

    probability distributions, movement on/off

--- mock/send_data.py
+++ mock/send_data.py
@@ -4,22 +4,38 @@ import requests
 import time
 from random import randint
 import threading
+from scipy.stats import norm
+
+def sleep(secs):
+    print("sleeping", secs)
+    time.sleep(secs)
+
+def daytime_as_float():
+    t = time.localtime()
+    return t.tm_hour + t.tm_min / 60. + t.tm_sec / 3600.
+
+def data_gen(device, peaks, stddev):
+    place, device = device
+    distributions = [norm(loc=peak, scale=stddev) for peak in peaks]
+    while True:
+        prob = sum(d.pdf(daytime_as_float()) for d in distributions)
+        print("current movement probability is", prob)
+        perform_time = randint(30,300)
+        idle_time = perform_time / prob
+        yield dict(place_id=place, device_id=device, type="movement", value=1)
+        sleep(perform_time)
+        yield dict(place_id=place, device_id=device, type="movement", value=0)
+        sleep(min(idle_time, 3600))
+
+def send_data_from_generator(url, data_generator):
+    for event in data_generator:
+        print(url, event)
+        #requests.post(url, json=event)
 
 def start_thread(function, args):
     t = threading.Thread(target=function, args=args)
     t.start()
     return t
-
-def data_gen(device, peaks, stddev):
-    place, device = device
-    while True:
-        yield dict(place_id=place, device_id=device, type="movement", value=1)
-        time.sleep(randint(15,30))
-
-def send_data_from_generator(url, data_generator):
-    for event in data_generator:
-        #requests.post(url, json=event)
-        print(url, event)
 
 if __name__ == '__main__':
     for device in config.devices:
