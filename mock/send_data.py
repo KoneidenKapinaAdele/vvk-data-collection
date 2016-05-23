ab4d776b15e17072769f4e9e4d9f42fd6578168a
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2016-05-23 16:21:01 +0300

    factor probability generation into separate function

--- mock/send_data.py
+++ mock/send_data.py
@@ -15,11 +15,14 @@ def daytime_as_float():
     t = time.localtime()
     return t.tm_hour + t.tm_min / 60. + t.tm_sec / 3600.
 
-def data_gen(device, peaks, stddev):
-    place, device = device
+def occu_probability_calculator(peaks, stddev):
     distributions = [norm(loc=peak, scale=stddev) for peak in peaks]
+    return lambda moment: sum(d.pdf(moment) for d in distributions)
+
+def data_gen(device, prob_fn):
+    place, device = device
     while True:
-        prob = sum(d.pdf(daytime_as_float()) for d in distributions)
+        prob = prob_fn(daytime_as_float())
         print("current probability of being occupied is", prob)
         perform_time = randint(30,300)
         # prob = pt / (pt+it) <=> it = pt/prob - pt
@@ -44,5 +47,7 @@ if __name__ == '__main__':
     for device in config.devices:
         start_thread(send_data_from_generator,
                 (config.back_url + config.event_endpoint,
-                    data_gen(device, config.rush_hours, config.deviation)))
+                    data_gen(device,
+                        occu_probability_calculator(config.rush_hours,
+                            config.deviation))))
 
