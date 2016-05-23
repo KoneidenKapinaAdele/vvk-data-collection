af5529ee1709c96d1ad649ecffd6f01a5f66dcef
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2016-05-23 16:06:27 +0300

    aamun ja illan ruuhkahuiput

--- mock/config.py
+++ mock/config.py
@@ -2,6 +2,6 @@
 back_url = "https://secret-oasis-98680.herokuapp.com"
 event_endpoint = "/v1/event"
 devices = [(1, 133700), (2, 133701), (3, 133702)]
-rush_hours = [12, 14]
+rush_hours = [8.5, 12, 14, 16.5]
 deviation = 0.7
 
