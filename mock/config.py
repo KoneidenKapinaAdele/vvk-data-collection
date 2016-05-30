2aaa31be7934ac2d6e35e3a95ef9fcd1b69a7d4e
Panu Kalliokoski <atehwa@azura.verkkotammi>
2016-05-30 09:43:29 +0300

    move mock data to places 7,8,9

--- mock/config.py
+++ mock/config.py
@@ -1,7 +1,7 @@
 
 back_url = "https://secret-oasis-98680.herokuapp.com"
 event_endpoint = "/v1/event"
-devices = [(1, 133700), (2, 133701), (3, 133702)]
+devices = [(7, 133700), (8, 133701), (9, 133702)]
 rush_hours = [8.5, 12, 14, 16.5]
 deviation = 0.7
 
