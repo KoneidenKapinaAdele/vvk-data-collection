7db59806e0468394288be15c95bd06e64db16240
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2016-06-07 01:03:02 +0300

    fix OAuth to work in parameterised API calls

--- telldus-client/apicall.py
+++ telldus-client/apicall.py
@@ -8,18 +8,19 @@ from requests import get
 consumer = oauth.OAuthConsumer(public_key, private_key)
 token = oauth.OAuthToken(token, token_secret)
 
-def request(url):
+def request(url, params):
     url = 'http://api.telldus.com/json/' + url
     oauth_req = oauth.OAuthRequest.from_consumer_and_token(consumer,
-            token=token, http_method='GET', http_url=url)
+            token=token, http_method='GET', http_url=url, parameters=params)
     oauth_req.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(),
             consumer, token)
     headers = oauth_req.to_header()
-    return get(url, headers=headers).json()
+    return get(url, headers=headers, params=params).json()
 
 if __name__ == '__main__':
-    result = request(sys.argv[1])
+    params = dict(arg.split('=') for arg in sys.argv[2:])
+    result = request(sys.argv[1], params)
     json.dump(result, sys.stdout, sort_keys=True,
             indent=4, separators=(',', ': '))
-    print()
+    print("")
 
