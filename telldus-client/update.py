c14d7fead89723e4ccd687cd0a861196d2e158c0
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2016-06-10 09:23:55 +0300

    small fixes: not re-emitting last event many times, and properly telling errors

--- telldus-client/update.py
+++ telldus-client/update.py
@@ -1,7 +1,7 @@
 
 import sys, requests
 
-from config import vvk_url
+from config import event_post_url
 from query_telldus import devices, new_events, convert_event, latest_timestamp
 from time import sleep
 from requests.exceptions import RequestException
@@ -9,12 +9,14 @@ from requests.exceptions import RequestException
 def send_event(event):
     print("%s %s is %s at %s" %
             (event['type'], event['place_id'], event['value'], event['time']))
-    try: return requests.post(vvk_url, json=event)
+    try: return requests.post(event_post_url, json=event)
     except RequestException as e: log("Post problem:", e)
 
 def fetch_and_send(since):
     events = new_events(devices(), since)
-    for event in events: print(send_event(convert_event(event)).status_code)
+    for event in events:
+        response = send_event(convert_event(event))
+        print("%d: %s" % (response.status_code, response.text))
     return latest_timestamp(events, since)
 
 def update_loop(interval, since=0):
@@ -25,5 +27,5 @@ def update_loop(interval, since=0):
         sleep(interval)
 
 if __name__ == '__main__':
-    update_loop(10, 1465486627)
+    update_loop(10, 1465538945)
 
